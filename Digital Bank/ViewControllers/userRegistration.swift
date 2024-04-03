4600da57ad80cf72e0823b79b13b1a44bc7d9b82
keithpuzey <keithpuzey@users.noreply.github.com>
2024-04-03 10:48:11 -0400

    Form update before IPA added

--- Digital Bank/ViewControllers/userRegistration.swift
+++ Digital Bank/ViewControllers/userRegistration.swift
@@ -10,15 +10,18 @@ import UIKit
 class userRegistration: UIViewController {
 
 
- 
-    
-//    @IBOutlet weak var regPhoneNumber: UITextField!
-
-
- 
-
     override func viewDidLoad() {
+        
+        // Enable secure text entry for password field
+        regPasswordTextField.isSecureTextEntry = true
+        // Set the date picker mode to date only
+        regDobPicker.datePickerMode = .date
+        
+        // Set locale to enforce MM/DD/YYYY format
+        regDobPicker.locale = Locale(identifier: "en_US_POSIX")
+        
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -48,10 +51,20 @@ class userRegistration: UIViewController {
     @IBOutlet weak var regPhoneTextField: UITextField!
     
     @IBOutlet weak var regAccept: UISwitch!
+    
+    
     @IBAction func signUpButtonTapped(_ sender: Any) {
+
+        // Check if the license agreement is accepted
+        guard isLicenseAgreementAccepted() else {
+            print("Please accept the license agreement.")
+            showAlert(title: "Error", message: "Please Accept the License Agreement")
+            return
+        }
+
         // Validate form before proceeding
-        if isFormValid() && isLicenseAgreementAccepted() {
-            
+        let validationResult = isFormValid()
+        if validationResult.isValid {
             // Collect data from form fields
             let titleIndex = regTitle.selectedSegmentIndex
             let title = regTitle.titleForSegment(at: titleIndex) ?? ""
@@ -62,6 +75,11 @@ class userRegistration: UIViewController {
             let lastName = regLNameTextField.text ?? ""
 
             let dob = regDobPicker.date // Get selected date from date picker
+            // Format the date
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yyyy"
+            let formattedDOB = dateFormatter.string(from: dob)
+            print("Date of Birth: \(formattedDOB)")
             let ssn = regSSNTextField.text ?? ""
             let email = regEmailTextField.text ?? ""
             let password = regPasswordTextField.text ?? ""
@@ -74,36 +92,119 @@ class userRegistration: UIViewController {
             print("Form is valid")
             
             // Call API with collected data
-            callAPI(title: title, gender: gender, firstName: firstName, lastName: lastName, ssn: ssn, email: email, password: password, address: address, region: region, locality: locality, postalCode: postalCode, phone: phone, dob: dob)
+            callAPI(title: title, gender: gender, firstName: firstName, lastName: lastName, ssn: ssn, email: email, password: password, address: address, region: region, locality: locality, postalCode: postalCode, phone: phone, formattedDOB: formattedDOB )
         } else {
             // Display error message or handle invalid form
-            print("Please fill in all fields and accept terms.")
+            let validationResult = isFormValid()
+            showAlert(title: "Form Validation Error", message: "\(validationResult.errorMessage)")
+                      
+            print("Form is invalid: \(validationResult.errorMessage)")
         }
+
     }
     
     // MARK: - Form Validation
     
-    func isFormValid() -> Bool {
-        // Check if all fields are filled and terms are accepted
-        print("Entering is valid funciton")
-        return !(regFNameTextField.text?.isEmpty ?? true) &&
-               !(regLNameTextField.text?.isEmpty ?? true) &&
-               !(regSSNTextField.text?.isEmpty ?? true) &&
-               !(regEmailTextField.text?.isEmpty ?? true) &&
-               !(regPasswordTextField.text?.isEmpty ?? true) &&
-               !(regAddressTextField.text?.isEmpty ?? true) &&
-                !(regRegionTextField.text?.isEmpty ?? true) &&
-                !(regLocalTextField.text?.isEmpty ?? true) &&
-               !(regPostalCodeTextField.text?.isEmpty ?? true)
-                    &&
-                !(regPhoneTextField.text?.isEmpty ?? true)
+    func isFormValid() -> (isValid: Bool, errorMessage: String) {
+          // Check if all fields are filled and terms are accepted
+           
+          // Check each field for validity
+          if regFNameTextField.text?.isEmpty ?? true {
+              return (false, "First Name is required")
+          }
+          if regLNameTextField.text?.isEmpty ?? true {
+              return (false, "Last Name is required")
+          }
+          if regSSNTextField.text?.count != 9 {
+              return (false, "SSN must be 9 digits long")
+          }
+        // Validate email format
+        if let email = regEmailTextField.text, !isValidEmail(email) {
+            return (false, "Invalid email format")
+        }
+        if (regPasswordTextField.text?.count ?? 0) < 8 {
+            return (false, "Password must be at leat 8 characters and contain at least one uppercase, one lowercase and one special character")
+        }
+        if !containsUppercaseLowercaseAndSpecialCharacter(regPasswordTextField.text ?? "") {
+            return (false, "Password must be at leat 8 characters and contain at least one uppercase, one lowercase and one special character")
+        }
+        if regAddressTextField.text?.isEmpty ?? true  {
+            return (false, "Address is Required")
+        }
+        if regRegionTextField.text?.isEmpty ?? true  {
+            return (false, "Region is Required")
+        }
+        if regLocalTextField.text?.isEmpty ?? true  {
+            return (false, "Locality is Required")
+        }
         
+        if regPostalCodeTextField.text?.isEmpty ?? true {
+            return (false, "Zip code  is required")
+        }
+        if regPhoneTextField.text?.isEmpty ?? true {
+            return (false, "Phone Number is required")
+        }
+        if let postalCode = regPostalCodeTextField.text, !isNumeric(postalCode) {
+            return (false, "Postal code must contain only numbers")
+        }
+        if let phone = regPhoneTextField.text, !isNumeric(phone) {
+            return (false, "Phone number must contain only numbers")
+        }
+          
+          // If all checks pass, return true
+          return (true, "")
+      }
+  
+    func containsUppercaseLowercaseAndSpecialCharacter(_ password: String) -> Bool {
+        let uppercaseLetterRegEx = ".*[A-Z]+.*"
+        let lowercaseLetterRegEx = ".*[a-z]+.*"
+        let specialCharacterRegEx = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]+.*"
+        let passwordLength = password.count
+
+        guard passwordLength >= 8 else {
+            return false // Password length must be at least 8 characters
+        }
+
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", uppercaseLetterRegEx)
+        let lowercasePredicate = NSPredicate(format: "SELF MATCHES %@", lowercaseLetterRegEx)
+        let specialCharacterPredicate = NSPredicate(format: "SELF MATCHES %@", specialCharacterRegEx)
+
+        return passwordPredicate.evaluate(with: password) &&
+               lowercasePredicate.evaluate(with: password) &&
+               specialCharacterPredicate.evaluate(with: password)
     }
 
+
+
+
+    func isNumeric(_ input: String) -> Bool {
+        return CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: input))
+    }
+    
+        func isValidEmail(_ email: String) -> Bool {
+            // Regular expression pattern for email validation
+            let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+
+            // Create a regular expression object
+            guard let regex = try? NSRegularExpression(pattern: emailRegex) else {
+                return false
+            }
+
+            // Match the regular expression pattern against the email string
+            let matches = regex.matches(in: email, range: NSRange(location: 0, length: email.utf16.count))
+            return !matches.isEmpty
+        }
+    
     
     func isLicenseAgreementAccepted() -> Bool {
         // Check if the license agreement switch is turned on
         return regAccept.isOn
+    }
+
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 
     // MARK: - API Call
@@ -111,7 +212,7 @@ class userRegistration: UIViewController {
 
 
 
-    func callAPI(title: String, gender: String, firstName: String, lastName: String,   ssn: String, email: String, password: String, address: String, region: String, locality: String, postalCode: String, phone: String, dob: Date )
+    func callAPI(title: String, gender: String, firstName: String, lastName: String,   ssn: String, email: String, password: String, address: String, region: String, locality: String, postalCode: String, phone: String, formattedDOB: String )
     {
         // Perform your API call here using the collected data
         print("API Call:")
@@ -128,7 +229,7 @@ class userRegistration: UIViewController {
         print("Phone: \(phone)")
         print("Title: \(title)")
         print("Gender: \(gender)")
-        print("Date of Birth: \(dob)")    }
+        print("Date of Birth: \(formattedDOB)")    }
     
 
  }
